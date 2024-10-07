6efd4aa34aa8bfa0c296e7a1ca96e16b6f28f340
Blake <Blakem2020@gmail.com>
2024-10-07 12:27:31 +0100

    Display cat info successfully displays the names of the mother cats and stores the total number of kittens
    TODO: refactor motherInfo so that it is allocated using a loop rather than hard coded with indexes
    
    TODO: the mother cats sentence should have a comma between each cat and an 'and' before the last name - regardless of how many cats there are

--- exercise/index.js
+++ exercise/index.js
@@ -17,8 +17,31 @@ function displayCatInfo(catString) {
 
   // Add your code here
   let catObj = JSON.parse(catString);
-  motherInfo += `${catObj[0].name}, ${catObj[1].name}, ${catObj[2].name}`;
 
+  // Adds the names of the mother cats
+  motherInfo += `${catObj[0].name}, ${catObj[1].name} and ${catObj[2].name}.`;
+
+  // Adds the total amount of kittens
+
+  // Outer loop goes over each mother cat within the catObj
+  for (let key in catObj) {
+    // Inner loop goes over the properties of each mother cat
+    for (let innerkey in catObj[key]) {
+      // Isolates the kitten array
+      if (Array.isArray(catObj[key][innerkey]) == true) {
+        for (let kitten in catObj[key][innerkey]) {
+          console.log(catObj[key][innerkey][kitten]);
+          total += 1;
+          if (catObj[key][innerkey][kitten].gender == "m") {
+            male += 1;
+          }
+        }
+      }
+    }
+  }
+
+  console.log(total);
+  console.log(male);
   // Don't edit the code below here!
 
   para1.textContent = motherInfo;
